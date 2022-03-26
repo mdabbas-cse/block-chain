c6ed6fe4f577db837db1b4704485b39c77cb9b46
mdabbas-cse <gmabbas44@gmail.com>
2022-03-26 16:57:59 +0600

    add comment for method description

--- block/main.js
+++ block/main.js
@@ -1,4 +1,10 @@
 const SHA256 = require('crypto-js/sha256')
+
+/**
+ * Create Class for new Block
+ * @params {index, timestamp, data, previousHash}
+ * @returns @Object
+ */
 class Block {
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index
@@ -8,11 +14,15 @@ class Block {
     this.hash = this.calculateHash()
   }
 
+  // form generate sha256 hash
   calculateHash() {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
   }
 }
 
+/**
+ * Create BlockChain
+ */
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()]
@@ -22,16 +32,19 @@ class BlockChain {
     return new Block(0, '01/01/2017', 'this is genesis block', '0')
   }
 
+  // for latest Block
   getLatestBlock() {
     return this.chain[this.chain.length - 1]
   }
 
+  // for add new Block
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash
     newBlock.hash = newBlock.calculateHash()
     this.chain.push(newBlock)
   }
 
+  // for check valid block-chain
   isValidChain() {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i]
